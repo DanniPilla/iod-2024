eadfd790eb932e9040a74c81bdf5e3f2a42d1c7b
DanniPilla <daniellapilla08@gmail.com>
2024-12-09 20:58:35 +1100

    testing workflow

--- Module 9/sockets-app/index.js
+++ Module 9/sockets-app/index.js
@@ -60,3 +60,6 @@ io.on("connection", (socket) => {
 server.listen(3000, () => {
   console.log("listening on *:3000");
 });
+app.listen(8080, () => {
+  console.log("Server is running on port 8080");
+});
